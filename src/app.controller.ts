e63712a9d5973b7eb2a326f95bd9d7cabb0538e6
ps-lizarazo <ps.lizarazo@uniandes.edu.co>
2022-11-27 14:14:18 -0500

    experimento

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,89 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Connection, Request } from 'tedious';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    // Experimentation, anti pattern
-    // Create connection to database and read some data
-    // This is not a good practice, but it's just for demonstration
-    return this.appService.getHello();
+  async getHello() {
+    const response = await this.getHello2();
+    return response;
+  }
+
+  @Get('2')
+  getHello3() {
+    return 'Crashed';
+  }
+
+  getHello2(): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      const config = {
+        server: '<ServerName>', //update me
+        authentication: {
+          type: 'default',
+          options: {
+            userName: 'Administrador', //update me
+            password: '<Password>', //update me
+          },
+        },
+        options: {
+          // If you are on Microsoft Azure, you need encryption:
+          encrypt: true,
+          database: 'experimento-failover-db', //update me
+        },
+      };
+      let connection: Connection;
+      try {
+        connection = new Connection(config);
+      } catch {
+        console.log('Error But no crash');
+        resolve(['Error']);
+      }
+
+      const request = new Request(
+        'SELECT TOP (10) * FROM [SalesLT].[vGetAllCategories];',
+        (err, rowCount) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(rowCount + ' rows');
+            resolve(response);
+          }
+        },
+      );
+      connection.on('connect', function (err) {
+        // If no error, then good to proceed.
+        if (err) {
+          console.log(err);
+          console.log('Hubo un error en la conexion, por favor no crashear');
+          reject(['Error']);
+          return;
+        }
+        console.log('Connected');
+        connection.execSql(request);
+      });
+
+      connection.connect();
+
+      const response = [];
+
+      request.on('row', function (columns) {
+        const obj = {};
+        columns.forEach(function (column) {
+          obj[column.metadata.colName] = column.value;
+        });
+        console.log(obj);
+        response.push(obj);
+      });
+
+      request.on('done', function () {
+        console.log('Done');
+        console.log(response.toString());
+        connection.close();
+        return response;
+      });
+    });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Connection, Request } from 'tedious';
 
 @Injectable()
 export class AppService {
+  connection: Connection;
   // Anti-pattern
   // En el constructor crear conexion a la bd
   getHello(): string {
-    return 'Hello World!';
+    const request = new Request(
+      'SELECT TOP (10) * FROM [SalesLT].[vGetAllCategories];',
+      (err, rowCount) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(rowCount + ' rows');
+        }
+      },
+    );
+
+    const response = [];
+
+    request.on('row', function (columns) {
+      columns.forEach(function (column) {
+        console.log(column.value);
+        response.push(column.value);
+      });
+    });
+
+    this.connection.execSql(request);
+    return response.toString();
   }
 }
